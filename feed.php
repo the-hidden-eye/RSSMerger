906a9feca3d226e1c1298ef0cf7438e351b818eb
the-hidden-eye <159456649+the-hidden-eye@users.noreply.github.com>
2024-02-12 16:12:49 +0100

    Update feed.php

--- feed.php
+++ feed.php
@@ -3,17 +3,26 @@ require_once "class_rssmerge.php";
 require_once "class_rssfeed.php";
 
 $rss = new RSSMerger();
-$rss->add("http://daverix.net/blog.rss");
-$rss->add("http://www.laurell.nu/feed.xml");
-$rss->sort();
+//$rss->add("http://daverix.net/blog.rss");
+//$rss->add("http://www.laurell.nu/feed.xml");
+$sources=$_GET['link'];
 
+//if(!is_array($sources))
+foreach(explode("_SPLITRSS_", $pizza) as $sourcelink) {
+    $rss->add($sourcelink);
+}
+$rss->sort();
 $xml = new RSSFeed("RSS merger","http://daverix.net/rssm/","a simple rss merge script","http://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF']);
 $xml->setLanguage("en");
+$feeds = $rss->getFeeds(99);
 
-$feeds = $rss->getFeeds(20);
+sentlinks=[];
 
 foreach($feeds as $f) {
-    $xml->addItem($f->title,$f->link,$f->description,$f->author,$f->guid,$f->time);
+    if(!in_array($f->link, sentlinks)) {
+        array_push($sentlinks,$f->link);
+        $xml->addItem($f->title,$f->link,$f->description,$f->author,$f->guid,$f->time);
+        }
 }
 $xml->displayXML();
 ?>
