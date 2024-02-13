f524db0a8cdd351cd9968a147f99aa3f7d5e6be8
the-hidden-eye <159456649+the-hidden-eye@users.noreply.github.com>
2024-02-13 12:03:23 +0000

    with '#' will be ignored, and an empty message aborts the commit.

--- sites/ksta.de/index.php
+++ sites/ksta.de/index.php
@@ -79,6 +79,7 @@ if (!file_exists("cache/")) {
 foreach ($doc->getElementsByTagName('item') as $node) {
   if($fetched < $maxfetch ) {
     $cache_file = "cache/" . md5($node->getElementsByTagName('link')->item(0)->nodeValue).".json";
+
     if(file_exists($cache_file)) {
         //we have a cached json
         $string = file_get_contents($cache_file); 
@@ -238,7 +239,7 @@ foreach ($doc->getElementsByTagName('item') as $node) {
 //print_r($arrFeeds);
 
 $feedtitle=xmlencode($feedtitle);
-$feedlink=$feedlink."?fromfeed=".$_GET['feed'];
+$feedlink=$feedtarget."/asd/".$feedlink."?fromfeed=".$_GET['feed'];
 header( "Content-type: text/xml; charset=UTF-8");
 //header('Content-Type: application/rss+xml; charset=UTF-8');
 echo "<?xml version='1.0' encoding='UTF-8'?>\r\n
